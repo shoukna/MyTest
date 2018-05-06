38998bf4485e269563c800e34861adef1c802012
shoukna <704331356@163.com>
2018-05-06 19:18:29 +0800

    modified:   isPalindrome.cpp 	modified:   reverse.cpp

--- isPalindrome.cpp
+++ isPalindrome.cpp
@@ -1,1 +1,33 @@
-id
+#include<cstdio>
+#include<string>
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main() {
+	int x;
+	cin>>x;
+	if(x<0) cout<<"false1";
+	int m,n=1;
+	int a[100];
+	int y=x;
+	int num=0;
+	for(int i=0;n!=0;i++){
+		m=y%10;
+		n=y/10;
+                y=n;
+		a[i]=m;
+		num++;
+		cout<<"a[i]:"<<a[i]<<"\n";
+	}
+	int b=0;
+	for(int i=num;i>0;i--){
+		b=b+a[i-1]*pow(10,num-i);
+	}
+	cout<<b;
+        if(b==x)
+		cout<<"true";
+        else cout<<"false"; 
+	return 0;
+}
+
+
--- reverse.cpp
+++ reverse.cpp
@@ -1,1 +1,37 @@
-ui
+#include<cstdio>
+#include<string>
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main() {
+    int x;
+    scanf("%d",&x);
+    if(x>(pow(2,31)-1)||x<(-pow(2,31))){
+        cout<<"error111";
+	return 0;
+    }
+    int a[100];
+    int m,n=1;
+    int i=0;
+    int num;
+    for(i=0,num=0;n!=0;i++){
+	    m=x%10; //最右的数
+            a[i]=m;
+            n=x/10;
+            x=n;
+            num++;
+            cout<<"a[i]"<<a[i];
+    }
+    int b=0;
+    for(int i=num;i>0;i--){
+	b=b+a[i-1]*pow(10,(num-i));
+    }
+    cout<<"b:"<<b<<"\n";
+    if(b>(pow(2,31)-1)||b<=(-pow(2,31))){
+	    cout<<"error222";
+	    return 0;
+    }
+    return 0;
+}
+
